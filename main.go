a9cd42c7e5a222ed5691936bc2e39efd7e01eaa0
deathzombee <gabrieldcalderon@gmail.com>
2020-09-17 11:36:00 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+
+	"log"
+	"math"
+)
+
+func main() {
+	s1 := 100
+	s2 := 100
+	s3 := 100
+	count := 0
+	for count < 10000001 {
+		rng1 := func() {
+			s1 = (171 * s1) % 30269
+			s2 = (172 * s2) % 30307
+			s3 = (170 * s3) % 30323
+
+		}
+
+		rng1()
+		u:=math.Mod(float64(s1)/30269.0 + float64(s2)/30307.0 + float64(s3)/30323.0, 1.0)
+		//fmt.Println(count, u)
+		log.Println(u)
+		count = count + 1
+	}
+}
